5e54666b6d5d616ed4f533328d24ae28f3859cc9
Cloudyy25 <168545967+Cloudyy25@users.noreply.github.com>
2024-06-25 18:35:44 +0700

    Update PeminjamanData.java

--- src/Fasilistis/PeminjamanData.java
+++ src/Fasilistis/PeminjamanData.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Fasilistis;
 
 import java.util.Date;
 
-/**
- *
- * @author Asus
- */
 public class PeminjamanData {
     private int id_peminjaman;
     private int user_id;
